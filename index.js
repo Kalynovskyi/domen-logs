d83da5531be4d0539d5940fe0ce98ecad77edb8d
Kalinovsky Nikita <nkalinovsky@gmail.com>
2024-02-22 14:51:41 +0200

    OK domains are removed

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-const { readFile } = require('fs/promises');
+const fs  = require('fs');
+const readline = require('readline');
 
-async function readThisFile(logFile) {
-    try {
+const fileStream = fs.createReadStream('logs/input-logs.txt');
 
+let blockedDomains = [];
+let i = 1;
 
-        const data = await readFile(logFile);
-        console.log(data.toString());
+const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+});
 
-        return data;
-    } catch (error) {
-        console.error(`Got an error trying to read the file: {error.message}` + error);
+rl.on('line', (line) =>{
+    
+    if (line !== "") {
+        isOk = line.includes('status: OK') || line.includes('Certificate did not match expected hostname')
+
+        if (!isOk) {
+            
+            let domain = line.slice(line.indexOf('domain:') + 7, line.indexOf('country:') - 2).trim();
+
+            let provider = line.slice(line.indexOf('country:'), line.indexOf('status:'));
+
+            console.log(i + " " + domain + "          " + provider);
+
+            i = i + 1;
+            //blockedDomains = '\n' + line;
+        }
     }
-}
+});
 
-readThisFile('logs/input-logs.txt');
+rl.on('close', () => {
+    console.log('Finished reading the file');
+});
+
+
+
+// async function readThisFile(logFile) {
+//     try {
+
+
+//         const data = await readFile(logFile);
+//         console.log(data.toString());
+
+//         return data;
+//     } catch (error) {
+//         console.error(`Got an error trying to read the file: {error.message}` + error);
+//     }
+// }
+
+// readThisFile('logs/input-logs.txt');
