34dbe4abe6b6b96f5e02cf80056e7d3c0d6f8a4f
Kalinovsky Nikita <nkalinovsky@gmail.com>
2024-02-27 12:57:14 +0200

    checkbox added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fs  = require('fs');
-const readline = require('readline');
+import * as fs from 'fs';
+import * as readline from 'readline';
 
 //add try/catch
 const fileStream = fs.createReadStream('logs/input-logs.txt');
@@ -15,7 +15,7 @@ const rl = readline.createInterface({
 
 rl.on('line', (line) =>{
     if (line !== "") {
-        isOk = line.includes('status: OK') || line.includes('Certificate did not match expected hostname') || line.includes('AM Domains list') || line.includes('PM Domains list')
+        let isOk = line.includes('status: OK') || line.includes('Certificate did not match expected hostname') || line.includes('AM Domains list') || line.includes('PM Domains list')
         if (!isOk) {
             let domain = line.slice(line.indexOf('domain:') + 7, line.indexOf('country:') - 2).trim();
             let domainLink = '';
@@ -24,9 +24,7 @@ rl.on('line', (line) =>{
                 domain = domain.slice(8);
             }
 
-            domainLink = `<a href='http://${domain}'>${domain}</a> <br>`;
-
-            console.log(domainLink);
+            domainLink = `<a href='http://${domain}'>${domain}</a>   <input type="checkbox"> <span>Is blocked</span><br>`;
 
             if (blockedDomains.indexOf(domain) == -1) {
                 let provider = line.slice(line.indexOf('country:'), line.indexOf('status:'));
@@ -38,14 +36,13 @@ rl.on('line', (line) =>{
 });
 
 setTimeout(function(){
-    //console.log(outputDomains);
     outputDomains = outputDomains.split('\n').sort().join('\n')
     outputDomains += "'\n'" + "Total domains: " + i;
 
     fs.writeFile('logs/output-logs.html', outputDomains, (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
-      }); 
+    }); 
 }, 3000);
 
 
@@ -55,4 +52,4 @@ rl.on('close', () => {
 
 
 
-//make it as browser page with links; Add sorting by provider
+//make it as browser page with links; Add sorting by provider/ Checkbox
